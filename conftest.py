f19c866254dd48745b8465f0a879c4892dff7879
michalbelicka <michal.belicka2310@gmail.com>
2026-07-17 14:02:28 +0200

    Add screenshot on failed selenium tests

--- conftest.py
+++ conftest.py
@@ -5,6 +5,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
+from datetime import datetime
 
 @pytest.fixture(scope="session")
 def driver():
@@ -23,3 +24,21 @@ def driver():
 def wait(driver):
     wait = WebDriverWait(driver, 10)
     return wait
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    report = outcome.get_result()
+
+    if report.when == "call" and report.failed:
+        driver = item.funcargs.get("driver")
+
+        if driver:
+            os.makedirs("reports/screenshots", exist_ok=True)
+
+            timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+            screenshot_path = (
+                f"reports/screenshots/{item.name}_{timestamp}.png"
+            )
+            driver.save_screenshot(screenshot_path)
